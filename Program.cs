199eca18b6dd5894ba802437f9e64b932368051e
ashkan amjad <amjad.ashkan@gmail.com>
2024-12-05 20:52:56 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Ashkan Motahari
+float score = 100F;
+if (score > 100)
+{
+    Console.WriteLine("Score is not Valid!");
+}
+else
+{
+    if (score > 80)
+    {
+        Console.WriteLine("Score A");
+    }
+    else
+    {
+        if(score >= 60)
+        {
+            Console.WriteLine("Score B");
+        }
+        else
+        {
+            if(score >= 40)
+            {
+                Console.WriteLine("Score C");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
